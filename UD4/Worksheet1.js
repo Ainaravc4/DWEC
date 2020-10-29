009e1811dbe2100f9d3d753c42529a384c7a14de
Ainara Vázquez <correobasuravc@gmail.com>
2020-10-29 09:59:04 +0100

    subiendo

--- UD4/Worksheet1.js
+++ UD4/Worksheet1.js
@@ -84,10 +84,4 @@ function moverImagen(e) {
   
 }
 
-/*var imagenes=document.querySelectorAll("img");
-imagenes.forEach(function(imagen){
-    imagen.addEventListener("dragend", function(e){
-        imagen.style.cssText="position: absolute; top: "+e.y+"; left: "+e.x+";";
-    });
-});*/
-//document.getElementById("imagen").onmousedown=moverImagen;
+
