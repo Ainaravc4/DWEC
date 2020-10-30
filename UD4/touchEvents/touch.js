c981e126e6c922162caf93374a6774fe1c6605f5
Ainara Vázquez <correobasuravc@gmail.com>
2020-10-30 12:24:58 +0100

    Update touch.js

--- UD4/touchEvents/touch.js
+++ UD4/touchEvents/touch.js
@@ -10,15 +10,15 @@ function startup() {
   window.onload=startup;
   function handleStart(evt) {
     evt.preventDefault();
-    document.getElementById("log").innerHTML="Touchstart"+evt.pageX+""+evt.pageY;
+    document.getElementById("log").innerHTML="Touchstart"+evt[0].pageX+""+evt[0].pageY;
 }
 function handleEnd(evt) {
     evt.preventDefault();
-    document.getElementById("log").innerHTML="Touchend";
+    document.getElementById("log").innerHTML="Touchend"+evt[0].pageX+""+evt[0].pageY;
 }
 function handleMove(evt) {
     evt.preventDefault();
-    document.getElementById("log").innerHTML="Touchmove";
+    document.getElementById("log").innerHTML="Touchmove"+evt[0].pageX+""+evt[0].pageY;
 }
 function click(){
     alert('click');
