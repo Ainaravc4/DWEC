d2e3751a5a4ad50ea83d45d18619122b41507ac6
Ainara Vázquez <correobasuravc@gmail.com>
2020-10-30 17:48:53 +0100

    booklist interfaz

--- UD4/touchEvents/touch.js
+++ UD4/touchEvents/touch.js
@@ -2,23 +2,23 @@ function startup() {
     var el = document.getElementsByTagName("canvas")[0];
     el.addEventListener("touchstart", handleStart, false);
     el.addEventListener("touchend", handleEnd, false);
-    el.addEventListener("touchcancel", handleCancel, false);
-    el.addEventListener("touchleave", handleLeave, false);
+    //el.addEventListener("touchcancel", handleCancel, false);
+    //el.addEventListener("touchleave", handleLeave, false);
     el.addEventListener("touchmove", handleMove, false);
-    el.addEventListener("click","alert('click')");
+    el.addEventListener("click",click);
   }
   window.onload=startup;
   function handleStart(evt) {
     evt.preventDefault();
-    document.getElementById("log").innerHTML="Touchstart"+evt[0].pageX+""+evt[0].pageY;
+    document.getElementById("log").innerHTML="Touchstart"+evt.pageX+""+evt.pageY;
 }
 function handleEnd(evt) {
     evt.preventDefault();
-    document.getElementById("log").innerHTML="Touchend"+evt[0].pageX+""+evt[0].pageY;
+    document.getElementById("log").innerHTML="Touchend"+evt.pageX+""+evt.pageY;
 }
 function handleMove(evt) {
     evt.preventDefault();
-    document.getElementById("log").innerHTML="Touchmove"+evt[0].pageX+""+evt[0].pageY;
+    document.getElementById("log").innerHTML="Touchmove"+evt.pageX+""+evt.pageY;
 }
 function click(){
     alert('click');
